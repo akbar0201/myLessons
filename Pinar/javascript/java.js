3cf54dc3ae470551d8c7996ab0a6caea70b58b85
akbar0201 <56400275+akbar0201@users.noreply.github.com>
2019-10-11 19:55:01 +0500

    Add files via upload

---
+++ Pinar/javascript/java.js
@@ -0,0 +1,15 @@
+// JavaScript Document
+function  openNavBar() {
+var navbar = document.getElementById('navbar-toggle').classList.toggle('navbar-toggle-open');
+var close = document.getElementById('nav-ul').classList.toggle('nav-ul-close');
+}
+	var hide = document.getElementById('hide');
+		function openFunction() {
+			if (hide.style.display === 'none'){
+				hide.style.display = "block";
+			}
+			else {
+				hide.style.display = 'none';
+			}
+			
+	}
